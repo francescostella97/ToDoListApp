4bcaa60a264238cdab63d32cf63c2fb39a0ee978
Francesco Stella <Francesco Stella>
2017-02-22 10:21:29 +0100

    UpdateCRUD

---
+++ app/src/main/java/com/example/utente/todolistapp/controllers/Utilities.java
@@ -0,0 +1,8 @@
+package com.example.utente.todolistapp.controllers;
+
+/**
+ * Created by Utente on 21/02/2017.
+ */
+
+public class Utilities {
+}
---
+++ app/src/main/java/com/example/utente/todolistapp/views/FAB_Hide_on_Scroll.java
@@ -0,0 +1,8 @@
+package com.example.utente.todolistapp.views;
+
+/**
+ * Created by Utente on 21/02/2017.
+ */
+
+public class FAB_Hide_on_Scroll {
+}
