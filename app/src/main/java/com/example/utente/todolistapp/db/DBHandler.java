e2717dcdb83a13217910592f21ea805f36fb7e44
Francesco Stella <Francesco Stella>
2017-02-24 22:08:06 +0100

    DB & CRUD up

---
+++ app/src/main/java/com/example/utente/todolistapp/db/DBHandler.java
@@ -0,0 +1,10 @@
+package com.example.utente.todolistapp.db;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Utente on 22/02/2017.
+ */
+
+public class DBHandler extends SQLiteOpenHelper {
+}
